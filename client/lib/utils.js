2cd4ab9160311b4b7c0f0c56495bfd91f5b21502
Ramadan E. Raji <armadanraji@gmail.com>
2024-06-15 12:48:30 -0500

    fixed path finder issues w/ utils,  Added modal for patient data in floor panel; fix link issues in discharge/update panel

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -4,3 +4,45 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+/************************************** */
+// Patient Utils
+/************************************** */
+
+export const findPatientData = (data, fullName) => {
+  return data.find((e) => e.fullName === fullName);
+};
+
+export const mapPatientField = (patientData, prefix) => {
+  const { fullName, roomId, currentChair } = patientData;
+  const [firstName, lastName] = fullName.split(" ");
+
+  // Return an object with the new field map object for each respective patient
+  return {
+    [`${prefix}_firstName`]: firstName,
+    [`${prefix}_lastName`]: lastName,
+    [`${prefix}_roomId`]: roomId,
+    [`${prefix}_currentChair`]: currentChair,
+  };
+};
+
+export const swapPatientData = (data, allPatientData) => {
+  const { patient1, patient2 } = data;
+
+  // Calling code to find patientData for the new patient1
+  const updatedPatient1Data = findPatientData(allPatientData, patient2);
+  const updatedPatient2Data = findPatientData(allPatientData, patient1);
+  // Calling code for creating a field map for the new swapped updated patients
+  const fieldMapForNewPatient1 = mapPatientField(
+    updatedPatient1Data,
+    "patient1",
+  );
+  const fieldMapForNewPatient2 = mapPatientField(
+    updatedPatient2Data,
+    "patient2",
+  );
+
+  return {
+    ...fieldMapForNewPatient1,
+    ...fieldMapForNewPatient2,
+  };
+};
